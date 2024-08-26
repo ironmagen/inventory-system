0aa8f501f881bf11541deb17cab40a5e4f7bc5c1
Thomas Hagen <43868840+ironmagen@users.noreply.github.com>
2024-08-26 14:49:41 -0400

    Update flaskConfig.py

--- flaskConfig.py
+++ flaskConfig.py
@@ -13,9 +13,6 @@ DB_PORT = os.environ.get('DB_PORT', 5432)
 SECRET_KEY = os.environ.get('SECRET_KEY', 'your_secret_key')
 DEBUG = os.environ.get('DEBUG', True)
 
-# Other configurations
-ITEMS_PER_PAGE = 10
-
 # PostgreSQL connection string
 POSTGRES_CONNECTION_STRING = f"dbname={DB_NAME} user={DB_USER} password={DB_PASSWORD} host={DB_HOST} port={DB_PORT}"
 
